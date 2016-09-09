c64ba3668ed51b58b4445694be302d825cd3be1a
Jasmin Börsig <jasc@abasag.intra>
2016-09-09 16:23:02 +0200

    unit test for Importer class

--- src/test/java/de/abas/custom/spareParts/catalogue/importer/ImporterTest.java
+++ src/test/java/de/abas/custom/spareParts/catalogue/importer/ImporterTest.java
@@ -1,11 +1,29 @@
 package de.abas.custom.spareParts.catalogue.importer;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
 import org.junit.Test;
+
+import de.abas.erp.axi.event.EventException;
 
 public class ImporterTest {
 
+	Importer instance = new CSVImporter();
+
 	@Test
 	public void readFileFileNotFoundTest() {
+		try {
+			instance.readFile(new File("does/not/exist"));
+			fail("FileNotFoundException leading to EventException expected");
+		} catch (final EventException e) {
+			assertThat(e.getMessage(), is("File \"exist\" not found"));
+		}
 	}
+
+	// TODO: further testing
 
 }
