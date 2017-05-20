6b5fb29802a582cd21c456c63f8de45abfa8347d
jazz <jasmin.borsig@gmail.com>
2017-05-20 11:16:25 +0200

    move build to cloud jenkins

--- initGradleProperties.sh
+++ initGradleProperties.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-if [ Msys = '$(uname -o)2>/dev/null' ]; then #git for windows
-	DOCKER_HOST=$(docker-machine ip default)
-elif [ Darwin = "$(uname -s)" ]; then #mac
-	DOCKER_HOST=$(docker-machine ip default)
-else #other
-	DOCKER_HOST=$(hostname)
+if [ ! $DOCKER_ENV_HOST ]; then
+    if [ Msys = '$(uname -o)2>/dev/null' ]; then #git for windows
+        DOCKER_ENV_HOST=$(docker-machine ip default)
+    else #other
+        DOCKER_ENV_HOST=$(hostname)
+    fi
 fi
 pwd=$(pwd)
-WF_URL="http://$DOCKER_HOST:8088/engine-rest"
 
 find . -name "*.properties.template" | while IFS= read -r pathname; do
     dirname=$(dirname "$pathname")
-    sed "s+DOCKER_HOST+$DOCKER_HOST+g; s+WF_URL+$WF_URL+g" "$pathname" > "$dirname/gradle.properties"
+    sed "s+DOCKER_ENV_HOST+$DOCKER_ENV_HOST+g" "$pathname" > "$dirname/gradle.properties"
 done
